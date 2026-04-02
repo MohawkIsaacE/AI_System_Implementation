5ac8782746500f10e79839089102ee17b3168a43
Isaac <isaac.ebert@mohawkcollege.ca>
2026-04-02 17:16:01 -0400

    Add player input

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,18 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum State { Idle, Wander, Graze, Alert, Flee, ReturnHome }
+
+    [Header("Scene References")]
+    public GameObject player;
+    public Transform[] wanderWaypoints;
+    public TextMeshProUGUI stateText;
+
+    [Header("Config")]
+    public float speedNormal;
+    public float speedFlee;
+
+    [Header("Vision")]
+    public float viewRadius = 10f;
+    [Range(0, 360)] public float viewAngle = 290f;
+
+    State state;
+
+    #region Unity Functions
+    private void Start()
+    {
+        // Always start in the idle state
+        state = State.Idle;
+    }
+
+    void Update()
+    {
+        // States
+        switch (state)
+        {
+            case State.Idle:
+                break;
+
+            case State.Wander:
+                break;
+
+            case State.Graze:
+                break;
+
+            case State.Alert:
+                break;
+
+            case State.Flee:
+                break;
+
+            case State.ReturnHome:
+                break;
+
+            default:
+                Debug.Log("Error [StateMachine]: Missing state");
+                break;
+        }
+    }
+    #endregion
+
+    #region State Machine
+    void EnterIdle()
+    {
+        state = State.Idle;
+    }
+    void Idle()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void EnterWander()
+    {
+        state = State.Wander;
+    }
+    void Wander()
     {
         
     }
+
+    void EnterGraze()
+    {
+        state = State.Graze;
+    }
+
+    void Graze()
+    {
+        
+    }
+
+    void EnterAlert()
+    {
+        state = State.Alert;
+    }
+
+    void Alert()
+    {
+        
+    }
+
+    void EnterFlee()
+    {
+        state = State.Flee;
+    }
+
+    void Flee()
+    {
+        
+    }
+
+    void EnterReturnHome()
+    {
+        state = State.ReturnHome;
+    }
+
+    void ReturnHome()
+    {
+        
+    }
+    #endregion
+
+    #region Support Methods
+    
+    #endregion
 }
